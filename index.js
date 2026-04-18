383caf1e866b095c897cbecbb1ddf09fbd427ce6
TimCodes <hardtimo@isu.edu>
2026-04-17 19:10:56 -0700

    chore: add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// yggdrasil - VulnFix POC test app
+
+const Handlebars = require('handlebars');
+const picomatch = require('picomatch');
+
+console.log('yggdrasil running - vulnerable deps: handlebars@4.7.8, picomatch@2.3.1');
